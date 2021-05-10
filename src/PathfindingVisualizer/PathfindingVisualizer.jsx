b313059bb7a5497ec75073e09e4095dc824f1e31
Pakome <maxime.lardier@epitech.eu>
2021-05-10 19:24:59 +0200

    fix: can't add wall while is running

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -9,8 +9,10 @@ const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
 export default class PathfindingVisualizer extends Component {
+  
   constructor(props) {
     super(props);
+    this.isRunning = false;
     this.state = {
       grid: [],
       mouseIsPressed: false,
@@ -23,18 +25,25 @@ export default class PathfindingVisualizer extends Component {
   }
 
   handleMouseDown(row, col) {
+    if (this.isRunning) return;
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
     this.setState({grid: newGrid, mouseIsPressed: true});
   }
 
   handleMouseEnter(row, col) {
-    if (!this.state.mouseIsPressed) return;
+    if (!this.state.mouseIsPressed || this.isRunning) return;
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
     this.setState({grid: newGrid});  
   }
 
   handleMouseUp() {
     this.setState({mouseIsPressed: false});
+  }
+
+  refreshGrid() {
+    // this.forceUpdate();
+    // this.setState(this.state);
+    window.location.reload(false);
   }
 
   animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
@@ -61,12 +70,14 @@ export default class PathfindingVisualizer extends Component {
   }
 
   visualizeDijkstra() {
+    this.isRunning = true;
     const {grid} = this.state;
     const startNode = grid[START_NODE_ROW][START_NODE_COL];
     const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
     const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
     this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+    this.running = false;
   }
 
   render() {
@@ -76,6 +87,9 @@ export default class PathfindingVisualizer extends Component {
       <>
         <button onClick={() => this.visualizeDijkstra()}>
           Visualize Dijkstra's Algorithm
+        </button>
+        <button onClick={() => this.refreshGrid()}>
+          Reset grid
         </button>
         <div className="grid">
           {grid.map((row, rowIdx) => {
